c3cb473746683ac4ce7f6caab7cf5ed48dcd4ec5
root <vishnutheja88@gmail.com>
2020-04-30 11:11:51 +0530

    second

---
+++ src/main/java/com/example/packtpub_devops/PacktpubDevopsApplication.java
@@ -0,0 +1,13 @@
+package com.example.packtpub_devops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PacktpubDevopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PacktpubDevopsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/packtpub_devops/PacktpubDevopsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.packtpub_devops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PacktpubDevopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
